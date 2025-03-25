6c7640d1f222180e8bdcf1200e015e0c6f218abe
yogui <wnores@gmail.com>
2025-03-25 15:24:12 -0300

    Entrega 2

--- src/Repartido1_3.java
+++ src/Repartido1_3.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Repartido1_3 {
     public static void main(String[] args) {
 
-
+        Scanner leer = new Scanner(System.in);
+        System.out.print("ingrese el primer numero: ");
+        int numero1= leer.nextInt();
     }
 }
